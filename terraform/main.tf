73045ac5732d6d8b40fe4e3c1883e1fae12d449b
Prashant <113302801+prashantsingh34@users.noreply.github.com>
2025-06-08 15:13:07 +0530

    added verisons of terraform and archive

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = "1.12.1"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.94.1"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.7.1"
     }
   }
   backend "s3" {

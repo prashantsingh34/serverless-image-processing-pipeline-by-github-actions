b869726aa1f587bc9c1cfefa93bb42315d4ee8c6
Prashant <113302801+prashantsingh34@users.noreply.github.com>
2025-06-14 17:05:24 +0530

    updated code to handle ar only on change in libraries

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "env"{
     type= string
     description= "The env in which code is running, branch name, stagig and prod"
 }
+
+variable "layer_arn"{
+    type= string
+    description= "python dependency layer arn"   
+}
